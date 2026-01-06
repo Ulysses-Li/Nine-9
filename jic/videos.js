1cfd7c2652bbc35851fbc6d480f50b311f1238e0
Ulysses-Li <louis962911@gmail.com>
2026-01-06 11:47:22 +0800

    Add files via upload

--- jic/videos.js
+++ jic/videos.js
@@ -2,36 +2,91 @@
 // 觀看過的影片資料（只看，不輸入）
 
 window.VIDEO_DB = [
-      {
-        series: "1",
-        name: "Most Shops Don’t Use Tool Holders the Same Way",
-        date: "2026-01-05",
-        url: "https://youtu.be/z4_EGRBNhGc?si=azr1vtaUmLL8chpT"
-      },
-      {
-        series: "2",
-        name: "Unboxing the INSANE Göltenbodt Tooling System",
-        date: "",
-        url: "https://www.youtube.com/embed/CTO734Y5eEU"
-      },
-      {
-        series: "3",
-        name: "ISCAR TECH-TALK - WHISPERLINE Anti-Vibration Holder",
-        date: "",
-        url: "https://www.youtube.com/embed/HREEbCnDXWM?start"
-      },
-      {
-        series: "4",
-        name: "ISCAR TECH-TALK - CUT-V-GRIP Multi-Functional V-Shape Inserts",
-        date: "",
-        url: "https://www.youtube.com/embed/Oag-AwSQDgY"
-      },  
-      {
-        series: "5",
-        name: "Aircraft Camera Mount - Machining",
-        date: "",
-        url: "https://youtu.be/EDFEOYW0MFY?si=iiEMtAaGUvhBhMQl"
-      },  
+  {
+    series: "1",
+    name: "Most Shops Don’t Use Tool Holders the Same Way",
+    date: "2026-01-05",
+    url: "https://youtu.be/z4_EGRBNhGc?si=azr1vtaUmLL8chpT"
+  },
+  {
+    series: "2",
+    name: "Unboxing the INSANE Göltenbodt Tooling System",
+    date: "",
+    url: "https://www.youtube.com/embed/CTO734Y5eEU"
+  },
+  {
+    series: "3",
+    name: "ISCAR TECH-TALK - WHISPERLINE Anti-Vibration Holder",
+    date: "",
+    url: "https://www.youtube.com/embed/HREEbCnDXWM?start"
+  },
+  {
+    series: "4",
+    name: "ISCAR TECH-TALK - CUT-V-GRIP Multi-Functional V-Shape Inserts",
+    date: "",
+    url: "https://www.youtube.com/embed/Oag-AwSQDgY"
+  },
+  {
+    series: "5",
+    name: "Aircraft Camera Mount - Machining",
+    date: "",
+    url: "https://youtu.be/EDFEOYW0MFY?si=iiEMtAaGUvhBhMQl"
+  },
+  {
+    series: "6",
+    name: "HoCNC Milling Job Shop Work – Custom Dovetail Fixturing for High Production",
+    date: "",
+    url: "https://youtu.be/q7zEOxLzx4U?si=o7eZotMF83Euz1uD"
+  },
+  {
+    series: "7",
+    name: "CNC Milling Job Shop Work - Prototype Time!",
+    date: "",
+    url: "https://youtu.be/y5m2sUdnP6E?si=JnszM4Ca_pE_OnRI"
+  },
+  {
+    series: "8",
+    name: "Comparing Tool Holders: Nut and Collet, Hydraulic Clamping, Thermogrip, and Hydraulic Press Fit.",
+    date: "",
+    url: "https://youtu.be/pNIL2D3W3TY?si=xTwR-kC8_ghbxFU1"
+  },
+  {
+    series: "9",
+    name: "How to choose the right tool holder for your CNC Machine | DN Solutions",
+    date: "",
+    url: "https://youtu.be/n2J1Wlfw-Dw?si=ADCU3Lnu3jxJz0Ei"
+  },
+  {
+    series: "10",
+    name: "ER Collet Essentials. Do You Know? – Haas Automation Tip of the Day",
+    date: "",
+    url: "https://youtu.be/WKikm6cQKh0?si=7SnNfSqI2b2_G5-C"
+  },
+  {
+    series: "11",
+    name: "CNC Machining an aluminum part | Hermle C400 | SolidCAM | iMachining",
+    date: "",
+    url: "https://youtu.be/Djb-Akfz21g?si=Uyn1P7Nns4X84tCh" 
+  },
+  {
+    series: "12",
+    name: "CNC Milling Job Shop Work - Evicting Some Serious Metal From A Housing",
+    date: "",
+    url: "https://youtu.be/5L8dQr3GUM0?si=5VYWJ3oz0fpITU12"
+  },
+  {
+    series: "13",
+    name: "Feed Adjustments",
+    date: "",
+    url: "https://youtu.be/Cdv0TJXl928?si=nxZGkOw0au4EQ0fC"
+  },
+  {
+    series: "14",
+    name: "Chip Thinning & High-Feed Machining",
+    date: "",
+    url: "https://youtu.be/hBQRAclNSuo?si=aAvLltnbBsO4ap6I"
+  }
+  
 
 
 ];
